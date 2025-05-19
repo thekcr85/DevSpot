75316939ea9fd2be11f6923c9a3d03a03c453cc0
thekcr85 <thekcr85@gmail.com>
2025-05-19 11:40:56 +0200

    Add project files.

---
+++ DevSpot/Program.cs
@@ -0,0 +1,61 @@
+using DevSpot.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevSpot
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+			builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+				options.SignIn.RequireConfirmedAccount = false)
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			using (var scope = app.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+				var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+				if (!roleManager.RoleExistsAsync("Admin").Result)
+				{
+					var result = roleManager.CreateAsync(new IdentityRole("Admin")).Result;
+				}
+			}
+
+			app.UseHttpsRedirection();
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapRazorPages();
+
+			app.MapStaticAssets();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}")
+				.WithStaticAssets();
+
+			app.Run();
+		}
+	}
+}
