2d8d809fb4f120121cbec2db2e4df0a043bc2fcd
thekcr85 <thekcr85@gmail.com>
2025-05-19 17:29:43 +0200

    Cretaed generic IRepository<T> interface

---
+++ DevSpot/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace DevSpot.Repositories
+{
+	// Generic repository interface for CRUD operations
+	public interface IRepository<T> where T : class
+	{
+		Task<IEnumerable<T>> GetAllAsync();
+		Task<T> GetByIdAsync(int id);
+		Task AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(int id);
+
+	}
+}
