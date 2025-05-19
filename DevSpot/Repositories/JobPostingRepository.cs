0a4bfc05e3486e1f9173d8455ca21450afcde33f
thekcr85 <thekcr85@gmail.com>
2025-05-19 17:53:46 +0200

    Implemented CRUD operations asynchronous methods in JobPostingRepository.

--- DevSpot/Repositories/JobPostingRepository.cs
+++ DevSpot/Repositories/JobPostingRepository.cs
@@ -1,8 +1,10 @@
 ﻿using DevSpot.Data;
 using DevSpot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevSpot.Repositories
 {
+	// Repository for JobPosting entity
 	public class JobPostingRepository : IRepository<JobPosting>
 	{
 		private readonly ApplicationDbContext _context;
@@ -12,29 +14,47 @@ namespace DevSpot.Repositories
 			_context = context;
 		}
 
-		public Task AddAsync(JobPosting entity)
+		// CRUD operations for JobPosting entity
+		public async Task AddAsync(JobPosting entity)
 		{
-			throw new NotImplementedException();
+			await _context.JobPostings.AddAsync(entity);
+			await _context.SaveChangesAsync();
 		}
 
-		public Task DeleteAsync(int id)
+		public async Task DeleteAsync(int id)
 		{
-			throw new NotImplementedException();
+			var jobPosting = await _context.JobPostings.FindAsync(id);
+
+			if (jobPosting is null)
+			{
+				throw new KeyNotFoundException();
+			}
+
+			_context.JobPostings.Remove(jobPosting);
+			await _context.SaveChangesAsync();
 		}
 
-		public Task<IEnumerable<JobPosting>> GetAllAsync()
+		public async Task<IEnumerable<JobPosting>> GetAllAsync()
 		{
-			throw new NotImplementedException();
+			return await _context.JobPostings.ToListAsync();
 		}
 
-		public Task<JobPosting> GetByIdAsync(int id)
+		public async Task<JobPosting> GetByIdAsync(int id)
 		{
-			throw new NotImplementedException();
+			var jobPosting = await _context.JobPostings.FindAsync(id);
+
+			if (jobPosting is null)
+			{
+				throw new KeyNotFoundException();
+			}
+
+			return jobPosting;
 		}
 
-		public Task UpdateAsync(JobPosting entity)
+		public async Task UpdateAsync(JobPosting entity)
 		{
-			throw new NotImplementedException();
+			_context.JobPostings.Update(entity);
+			await _context.SaveChangesAsync();
 		}
 	}
 }
