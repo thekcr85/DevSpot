36ed1340265491c2e5e2ae609c982cbc7b2d5d04
thekcr85 <thekcr85@gmail.com>
2025-05-19 18:14:47 +0200

    Created new xUnit Test project called DevSpot.Tests. Created new class JobPostingRepositoryTests with Initial InMemory DbContext

---
+++ DevSpot.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using DevSpot.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSpot.Tests
+{
+	internal class JobPostingRepositoryTests
+	{
+		private readonly DbContextOptions<ApplicationDbContext> _options;
+
+		public JobPostingRepositoryTests()
+		{
+			_options = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseInMemoryDatabase("JobPostingDb")
+				.Options;
+		}
+
+		private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+	}
+}
